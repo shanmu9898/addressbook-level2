b974b0a849f30f9aeae5e79ed714c6eb5df5090b
shanmu9898 <shanmu9898@icloud.com>
2018-02-20 11:21:55 +0800

    Added tests for elements which are null and not null and a combination of both

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -36,6 +36,36 @@ public class UtilsTest {
         assertNotUnique(null, "a", "b", null);
     }
 
+    @Test
+    public void isanyNull() throws Exception {
+
+        //if none are null
+        assertNotNull();
+        assertNotNull("a");
+        assertNotNull("aa","aa");
+        assertNotNull("a","b","c");
+
+        //if object is null
+        assertIsNull((Object)null);
+
+        //if one is null
+        assertIsNull("a",null);
+        assertIsNull("a",1,null);
+
+        //if all are null
+        assertIsNull(null,null);
+        assertIsNull(null,null,null);
+    }
+
+
+    private void assertIsNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
